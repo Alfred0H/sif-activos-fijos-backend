e56067cc3d95055068587130ec58eb1b967e25d5
Alfred0H <jaherrerap@unah.hn>
2022-10-29 19:19:18 -0600

    Conexion a posgrest terminada

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import express from 'express'; //paquete para servidor web
+import Connection from './database/connection';
+
 class App{
 
     public express : express.Application;
+    private conecction : Connection | undefined; 
     constructor(){
         this.express = express();
+        this.db();
+
+    }
+
+    db(){
+        this.conecction = new Connection();
+        this.conecction.connection
+        .sync()
+        .then(()=>{
+            console.log(`Database is connected`);
+        })
+        .catch((err) => {
+            console.log(`Error`, err);
+        });
+
     }
     listen(port: number){
         this.express.listen(port,
